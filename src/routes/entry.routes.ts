84446a2d1f0e872167faf504ac8da347b11c5c18
vadimBRR <vadmabos20@gmail.com>
2025-06-22 23:43:17 +0200

    add comments

--- src/routes/entry.routes.ts
+++ src/routes/entry.routes.ts
@@ -4,9 +4,19 @@ import { Router } from 'express'
 
 const router = Router()
 
+
+// create entry
 router.post('/entries', createEntry)
+
+// get all entries
 router.get('/entries',getAllEntries)
+
+// Get entry by id
 router.get('/entries/:id', getEntryById)
+
+// delete entry by id
 router.delete('/entries/:id',deleteEntryById)
+
+// update entry by id
 router.put('/entries/:id',updateById)
 export const entryRouter = router
