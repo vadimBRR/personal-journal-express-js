86779109221710f67db3b8f6c5d11ff8fee20f38
vadimBRR <vadmabos20@gmail.com>
2025-06-23 12:33:41 +0200

    add comments

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -5,6 +5,9 @@ import jwt from 'jsonwebtoken'
 const JWT_SECRET = process.env.JWT_SECRET || 'supersecret'
 
 export class AuthService {
+
+	// hash password  & bcrypt.hash
+	// create user & prisma.user.create
 	async register(email: string, password: string) {
 		const hashed = await bcrypt.hash(password, 10)
 		return await prisma.user.create({
@@ -15,22 +18,24 @@ export class AuthService {
 		})
 	}
 
-  async login(email:string, password:string){
-    const user = await prisma.user.findUnique({where: {email}})
-    if(!user) throw new Error("User not found")
+	// get user         & prisma.user.findUnique
+	// check password   & bcrypt.compare
+  // generating token & jwt.sign({user}, jwt_secret, {options})
+	async login(email: string, password: string) {
+		const user = await prisma.user.findUnique({ where: { email } })
+		if (!user) throw new Error('User not found')
 
-    const isMatch = await bcrypt.compare(password, user.password);
-    if(!isMatch) throw new Error("Invalid password")
+		const isMatch = await bcrypt.compare(password, user.password)
+		if (!isMatch) throw new Error('Invalid password')
 
-    const token = jwt.sign({user:user.id}, JWT_SECRET, {expiresIn: '1h'})
+		const token = jwt.sign({ user: user.id }, JWT_SECRET, { expiresIn: '1h' })
 
-    return {token, user: {id:user.id, email:user.email}}
-
-  }
-
-  verifyToken(token:string){
-    return jwt.verify(token, JWT_SECRET)
-  }
+		return { token, user: { id: user.id, email: user.email } }
+	}
 
 
+  // verifying token &jwt.verify
+	verifyToken(token: string) {
+		return jwt.verify(token, JWT_SECRET)
+	}
 }
